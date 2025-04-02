ccc828e1dafea2c936f3d4d72081534050dec1e0
Chandrahash Kumar <chandrahashkumar256@gmail.com>
2025-04-03 00:05:01 +0530

    add gallery

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function toggleMenu() {
     console.error('Element with class "menu-links" not found.');
   }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const currentPage = window.location.pathname.split('/').pop();
+  const galleryLink = document.querySelector('a[href="gallery.html"]');
+  if (currentPage === 'gallery.html' && galleryLink) {
+    galleryLink.style.display = 'none';
+  }
+});
